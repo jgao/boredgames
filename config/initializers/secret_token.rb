4e80722ffbb87fe5a4347692c83fe079bb44ad9e
Jack Gao <jackgao2006@gmail.com>
2013-03-10 19:21:37 -0700

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Boredgames::Application.config.secret_token = '78c3cdaf5755e01d8abfcf7eb89f29fdc75d779d00e3641b86457b03202fb7f58021b7d62a3e9d490d7b016e093975ab7b7160b0dcc86b6fd9679971ebb06200'
